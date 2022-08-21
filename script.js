e4acb642f41346a3822481e8bafffba6782ed7f1
berry <kathborowy@gmail.com>
2022-08-21 23:40:42 +0200

    works on different sized sqrs

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 let cont = document.querySelector('div.grids')
 
-let gridNum = Math.pow(16, 2) 
-for (let x = 1; x <= gridNum; x++) {
-  let appendDiv = document.createElement('div')
-  appendDiv.classList.add('square');
-  cont.appendChild(appendDiv);
-}
-
-let mousePosition = false;
-window.onmousedown = () => (mousePosition = true,
-                                  console.log(mousePosition))
-window.onmouseup = () => (mousePosition = false,
-                                  console.log(mousePosition))
-console.log(mousePosition)
-
-
-sqrs.forEach(sq => {
+function createSquares(num) { 
+  // removes every previous square 
+  while (cont.hasChildNodes()) { 
+    cont.removeChild(cont.firstElementChild)
+  }
+  // creates num amount of squares
+  for (let x = 1; x <= Math.pow(num, 2); x++) {
+    let appendDiv = document.createElement('div')
+    appendDiv.classList.add('square');
+    cont.appendChild(appendDiv);
+  }
+  
+  let sqrs = document.querySelectorAll('div.square')
+  let grid = document.querySelector('div.grids')
+  
+  // var that determinates whether or not mouse is pressed 
+  let mousePosition = false;
+  grid.onmousedown = () => (mousePosition = true,
+                            console.log(mousePosition))
+  window.onmouseup = () => (mousePosition = false,
+                                    console.log(mousePosition))
+  sqrs.forEach(sq => {
     sq.addEventListener('mousemove', () => {
       if (mousePosition === true) {
+        console.log(sq)
         sq.style.cssText = 'background-color: black'
       }
-  })})
+    })
+  })
+}
+
+createSquares(20);
