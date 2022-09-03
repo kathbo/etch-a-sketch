7dfec6d3ae795082aedbb49326f5715b6170bdc3
berry <kathborowy@gmail.com>
2022-09-03 17:46:32 +0200

    script2 = old code

--- script2.js
+++ script2.js
@@ -1,53 +1,109 @@
-const sqrs = document.querySelectorAll('.grids > div');
-const grid = document.querySelector('div.grids');
+let sqrs = document.querySelectorAll('.grids > div')
+let grid = document.querySelector('div.grids')
+let resetBtn = document.querySelector('#reset');
 
-const smallBtn = document.querySelector('#smallBtn');
-const normalBtn = document.querySelector('#normalBtn');
-const bigBtn = document.querySelector('#bigBtn');
-const hugeBtn = document.querySelector('#hugeBtn');
 
-let currentSize = 16; // default
+// var that determinates whether or not mouse is pressed 
+let mousePosition = false;
+document.body.onmousedown = () => (mousePosition = true);
+document.body.onmouseup = () => (mousePosition = false);
 
-smallBtn.onclick = () => currentSize = 5;
-normalBtn.onclick = () => currentSize = 16;
-bigBtn.onclick = () => currentSize = 36;
-hugeBtn.onclick = () => currentSize = 64;
-
-// function that creates a desired sized grid
-function clearGrid() {
-    while (grid.hasChildNodes()) { 
-        grid.removeChild(grid.firstElementChild)
-    }
-}
-function insertSquares(size) {
-    for (let x = 1; x <= Math.pow(size, 2); x++) {
-        let appendDiv = document.createElement('div'); // ????????????????
-        grid.appendChild(appendDiv);
-    }
-      grid.style.cssText = `grid-template-columns: repeat(${size}, 1fr);`
-    
-}
-function createSquares(size) { 
-    clearGrid();
-    insertSquares(size);
-    
-  // colors a pressed square
-    document.querySelectorAll('.grids > div').forEach(sq => {
-      sq.addEventListener('mouseover', () => {
-        if (mousePosition === true) {
-          if (currentColor == "grey") {
-            sq.classList.add('clicked')
-          } 
-          if (currentColor == "rainbow") {
-            sq.style.cssText = `background-color: ${makeUpHEX()}`
-          }
-        }
-      })
-    })
+// size
+function createSquares(num) { 
+  // removes every previous square 
+  while (grid.hasChildNodes()) { 
+    grid.removeChild(grid.firstElementChild)
+  }
+  // creates num amount of squares
+  for (let x = 1; x <= Math.pow(num, 2); x++) {
+    let appendDiv = document.createElement('div')
+    //appendDiv.style.cssText = `border: 1px solid black`;
+    grid.appendChild(appendDiv);
   }
   
-const rainbowBtn = document.querySelector('#rainbowButton');
-const grayBtn = document.querySelector('#grayBtn');
-const eraserBtn = document.querySelector('#eraser');
-const resetBtn = document.querySelector('#reset');
+  document.querySelector('div.grids').style.cssText = `grid-template-columns: repeat(${num}, 1fr);`
 
+// colors a pressed square
+  document.querySelectorAll('.grids > div').forEach(sq => {
+    sq.addEventListener('mouseover', () => {
+      if (mousePosition === true) {
+        if (currentColor == "grey") {
+          sq.classList.add('clicked')
+        } 
+        if (currentColor == "rainbow") {
+          sq.style.cssText = `background-color: ${makeUpHEX()}`
+        }
+      }
+    })
+  })
+}
+
+let currentSize = 16; // default value
+createSquares(currentSize);
+
+let btnsSize = document.querySelectorAll('.btnSize');
+btnsSize.forEach(btn => {
+  btn.addEventListener('click', () => {
+    createSquares(currentSize)
+  })
+})
+
+// current mode
+let currentMode = 'color' // default
+
+
+
+
+// *** COLOR ***
+let currentColor = 'grey' // default
+
+function makeUpHEX() {
+  let hex = [];
+  let possibilities = "abcdef1234567890"
+  for (let x = 0; x < 6; x++) {
+    let randomNum = Math.floor(Math.random() * (15 - 0 + 1))
+    hex.push(possibilities[randomNum])
+  }
+  return '#' + hex.join('')
+}
+
+
+// *** REMOVAL ***
+// erase
+let eraseBtn = document.querySelector('#eraser')
+
+document.querySelectorAll('.grids > div').forEach(sq => {
+  sq.addEventListener('moveover', () => {
+    if (mousePosition === true) {
+      sq.style.cssText = "background-color: none"
+    }
+  })
+})
+// eraseBtn.addEventListener('click', () => {
+//   document.querySelectorAll('.grids > div').forEach(sq => {
+//     sq.addEventListener('mouseover', () => {
+//       if (mousePosition === true) {
+//         sq.style.cssText = "background-color: none"
+//       }
+//     })
+//   })
+// })
+
+// reset
+resetBtn.addEventListener('click', () => {
+  document.querySelectorAll('.grids > div').forEach(sq => {
+    sq.style.cssText = "background-color: none"
+  })
+})
+
+
+
+
+
+// delete section's p text
+// color of gray 
+
+// erase doesnt work properly
+// rainbow has a president over grey
+// distinguish which button is pressed
+// *this weird stoopid error????
